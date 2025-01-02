ace00bcbb0333745dbc74332af67291c91f1beb8
medi_rudiant <medirudiantoni2@gmail.com>
2025-01-02 13:22:36 +0700

    yang tadi kurang /api/ di branch main

--- src/utils/baseurl.ts
+++ src/utils/baseurl.ts
@@ -1,3 +1,3 @@
 export const apiURL =
-   import.meta.env.VITE_API_URL || 'https://b58-circle-backend.vercel.app/';
+   import.meta.env.VITE_API_URL || 'https://b58-circle-backend.vercel.app/api/';
 // import.meta.env.VITE_API_URL || 'http://localhost:5000/api/';
